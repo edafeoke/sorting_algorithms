ade83c8bc38e7b75f502eb36f5f2091719a1307c
Oke Edafe Great <greatedafeoke@gmail.com>
2021-10-25 17:27:12 -0700

    Corrected insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,7 +7,7 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *storeP, *storeN, *current, *previous;
+	listint_t *current, *previous, *start, *end;
 
 	if (*list == NULL)
 		return;
@@ -17,6 +17,20 @@ void insertion_sort_list(listint_t **list)
 		previous = current->prev;
 		while (previous && previous->n > current->n)
 		{
+			start = previous->prev;
+			end = current->next;
+			if (start)
+				start->next = current;
+			if (end)
+				end->prev = previous;
+			previous->next = end;
+			previous->prev = current;
+			current->next = previous;
+			current->prev = start;
+			if (!start)
+				*list = current;
+			print_list(*list);
+			previous = current->prev;
 		}
 		current = current->next;
 	}
